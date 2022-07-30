37522c455fda32fb07c1afa262c599c4ad163ae8
0brym <91846986+0brym@users.noreply.github.com>
2022-07-30 11:56:15 +0100

    Update h.js

--- h.js
+++ h.js
@@ -8,12 +8,7 @@ h.ref=()=>document.referrer;
 h.element = x => document.createElement(x);
 h.getEl = x => document.getElementById(x);
 h.val = x => h.getEl(x).value;
-h.addEl = (tag, id, where) => {
-  let t = h.getEl(where);
-  let x = h.element(tag);
-  x.id = id;
-  t.appendChild(x);
-}
+h.addEl = (tag, id, where) => {let t=h.getEl(where);let x=h.element(tag);x.id=id;t.appendChild(x);}
 h.set = (elem, data) => h.getEl(elem).innerHTML = data;
 h.add = (elem, msg) => !elem ? h.log(`Error (h.add): Element '${elem}' not found`) : h.getEl(elem).innerHTML += msg;
 h.remove = id => h.getEl(id).remove();
@@ -23,7 +18,7 @@ h.save = (a, b) => localStorage.setItem(a,b);
 h.get = x => localStorage.getItem(x);
 h.del = x => localStorage.removeItem(x);
 h.delAll = () => localStorage.clear();
-h.img = (img, alt) => return `<img src="${img}" alt="${alt}">`;
+h.img = (img, alt) => `<img src="${img}" alt="${alt}">`;
 h.meta=(a,b,c,d)=>{let x=h.element("META");x.setAttribute(a,b);if(c&&d){x.setAttribute(c,d);};document.head.appendChild(x);}
 h.link=(a,b,c,d,e,f,g,i)=>{
   let x=h.element("LINK");
@@ -36,4 +31,3 @@ h.link=(a,b,c,d,e,f,g,i)=>{
 h.css=path=>h.link("rel","stylesheet","href",path,"media","none","onload","if(media!='all')media='all'");
 h.imp=async(target,src)=>{fetch(src).then((res)=>res.text()).then((html)=>h.set(target,html)).catch((err)=>h.showToast(err));}
 h.elBg=(x,y,z)=>h.styles(x,`background-image:url("${y}");background-size:cover;background-position:center center;background-blend-mode:"${z}";`);
-

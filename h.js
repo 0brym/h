246bb6a3d568bb122d65d9afbc8f7612eeaec927
0brym <91846986+0brym@users.noreply.github.com>
2022-07-30 09:36:44 +0100

    Create h.js

---
+++ h.js
@@ -0,0 +1,35 @@
+// enjoy
+var h = h || {};
+h.log = x => console.log(x);
+h.msg = x => window.alert(x);
+h.pr = (x, y) => window.prompt(x,y);
+h.element = x => document.createElement(x);
+h.getEl = x => document.getElementById(x);
+h.val = x => h.getEl(x).value;
+h.addEl = (tag, id, where) => {
+  let t = h.getEl(where);
+  let x = h.element(tag);
+  x.id = id;
+  t.appendChild(x);
+}
+h.set = (elem, data) => h.getEl(elem).innerHTML = data;
+h.add = (elem, msg) => !elem ? h.log(`Error (h.add): Element '${elem}' not found`) : h.getEl(elem).innerHTML += msg;
+h.remove = id => h.getEl(id).remove();
+h.styles = (id, styles) => h.getEl(id).style = styles;
+h.classes = (id, classes) => h.getEl(id).className = classes;
+h.save = (a, b) => localStorage.setItem(a,b);
+h.get = x => localStorage.getItem(x);
+h.del = x => localStorage.removeItem(x);
+h.delAll = () => localStorage.clear();
+h.img = (img, alt) => return `<img src="${img}" alt="${alt}">`;
+h.meta=(a,b,c,d)=>{let x=h.element("META");x.setAttribute(a,b);if(c&&d){x.setAttribute(c,d);};document.head.appendChild(x);}
+h.link=(a,b,c,d,e,f,g,i)=>{
+  let x=h.element("LINK");
+  x.setAttribute(a,b);
+  x.setAttribute(c,d);
+  if(e&&f)x.setAttribute(e,f);
+  if(g&&i)x.setAttribute(g,i);
+  document.head.appendChild(x);
+}
+h.css=path=>h.link("rel","stylesheet","href",path,"media","none","onload","if(media!='all')media='all'");
+h.imp=async(target,src)=>{fetch(src).then((res)=>res.text()).then((html)=>h.set(target,html)).catch((err)=>h.showToast(err));}

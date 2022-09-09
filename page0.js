34810ed04a08f1883cf5eded384741207f4a83c9
0brym <dodgy.admin@gmail.com>
2022-09-09 21:24:09 +0100

    updated r.js to include some semantic html elements for basic structure, and added some basic content to page0.js

---
+++ page0.js
@@ -0,0 +1,8 @@
+document.body.style.backgroundColor="#343330";
+h.addEl("div","grid","main");
+h.styles("grid","font-family:'Ubuntu',Sans-Serif;color:white;");
+h.set("grid",`
+	<h2>Sample Heading</h2>
+	<p>Sample paragraph tag to slot some fancy looking Lorem Ipsums into.</p>
+	<p>Another paragraph tag because, well, why not!</p>
+`);
--- r.js
+++ r.js
@@ -1,3 +1,8 @@
+// lol
+let head=h.element("header");head.id="header";document.body.appendChild(head);
+let shoulders=h.element("main");shoulders.id="main";shoulders.setAttribute("role","main");document.body.appendChild(shoulders);
+let kneesntoes=h.element("footer");kneesntoes.id="footer";kneesntoes.setAttribute("role","footer");document.body.appendChild(kneesntoes);
+
 let s=h.site();
 let d=document.URL;
 switch (d) {

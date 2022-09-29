b27e7a4e2c628dde2c5515d2b076be7fffacc0be
0brym <dodgy.admin@gmail.com>
2022-09-29 08:39:12 +0100

    minor refactor

--- r.js
+++ r.js
@@ -3,8 +3,7 @@ let head=h.element("header");head.id="header";document.body.appendChild(head);
 let shoulders=h.element("main");shoulders.id="main";shoulders.setAttribute("role","main");document.body.appendChild(shoulders);
 let kneesntoes=h.element("footer");kneesntoes.id="footer";kneesntoes.setAttribute("role","footer");document.body.appendChild(kneesntoes);
 
-let s=h.site();
-let d=document.URL;
+let s,d;s=h.site();d=document.URL;
 switch (d) {
 	case s+'?page1':h.set("main",`Hello from Page 1. Kind of.`);break;
 	default:h.js("page0.js");break;
